e00921fe483ee2fe4bd3b9b6073421ba8f2260e6
Georges034302 <georges.boughantous@gmail.com>
2023-07-22 20:52:26 +1000

    lecture7

--- lectures/lecture7/people.py
+++ lectures/lecture7/people.py
@@ -1,59 +1,59 @@
-import person
+import person 
 
-def exist(data, key):
-    for id in data.keys():
-        if id == key:
+def exist(data,ID):
+    for key in data.keys():
+        if key == ID:
             return True
     return False
 
 def records():
     data = {}
-    first = int(input("first = "))
-    last = int(input("last = "))
-    n = int(input("size = "))
-    keys = person.unique_ids(first,last,n)
+    first = int(input("First = "))
+    last = int(input("Last = "))
+    size = int(input("Size = "))
+    keys = person.uniq_ids(first,last,size)
     data = person.records(keys)
     return data
 
-data = records() # data set
+data = records()
 
 def create():
-    key = int(input("key = "))    
-    if not(exist(data,key)):
-        value = input("value = ")
-        person.create(data,key,value)
-    else:
-        print("There is a record associated with ", key)
-
-def read():
-    key = int(input("key = "))
-    if exist(data,key):
-        print(f"{key} --> {data[key]}")
-    else:
-        print("No record associated with ", key)
-        
-def update():      
-    key = int(input("key = "))    
-    if exist(data,key):
-        value = input("value = ")
-        person.update(data, key, value)  
-    else:
-        print("No record associated with ", key)
-
-      
+    key = int(input("ID = "))
     
-def delete():
-    key = int(input("key = "))
-    if exist(data,key):
-        person.delete(data,key) 
+    if not(exist(data,key)):
+        value = input("Name: ")
+        person.create(data, key,value)
     else:
-        print("No record associated with ", key) 
-   
+        print("There is a record associated with the key ",key)
+        
+def read():
+    key = int(input("ID = "))
+    if exist(data,key):
+        print(f'{key} --> {person.read(data,key)}')
+    else:
+        print(f'no record associated with the key {key}')
+
+def update():
+    key = int(input("ID = "))
+    if exist(data,key):
+        value = input("Name: ")
+        person.update(data,key,value)
+    else:
+        print(f'no record associated with the key {key}')
+        
+def delete():
+    key = int(input("ID = "))
+    if exist(data,key):        
+        person.delete(data,key)
+    else:
+        print(f'no record associated with the key {key}')
+        
 def view():
     person.view(data)
-        
+    
 def menu():
-    choice = input("Choice(c/r/u/d/v/x): ")
+    choice = input("choice(c/r/u/d/v/x): ")
+    
     while choice != "x":
         match choice:
             case 'c': create()
@@ -62,5 +62,6 @@ def menu():
             case 'd': delete()
             case 'v': view()
             case _ : print("Unknown choice!")
-        choice = input("Choice(c/r/u/d/v/x): ")
-menu()
+        choice = input("choice(c/r/u/d/v/x): ")
+        
+menu()   
--- lectures/lecture7/person.py
+++ lectures/lecture7/person.py
@@ -1,7 +1,7 @@
-import random as ran
-import pprint as pp
+import random as ran 
+import pprint as pp 
 
-def unique_ids(first,last,size):
+def uniq_ids(first,last,size):
     return list(set(ran.sample(range(first,last),size)))
 
 def records(keys):
@@ -10,17 +10,18 @@ def records(keys):
         names[key] = input("Name: ")
     return names
 
-def create(names, key, value):
-        names[key] = value
+def create(names, key, name):
+    names[key] = name 
 
-def read(names,ID):
-    return names[ID]       
+def read(names, ID):
+    return names[ID]
 
-def update(names,key,value):
-    names[key] = value
-
-def delete(names,key):  
-        del names[key]
-        
+def update(names, ID, name):
+    names[ID] = name 
+    
+def delete(names, ID):
+    del names[ID]
+    
 def view(names):
     pp.pprint(names,width=20)
+    
